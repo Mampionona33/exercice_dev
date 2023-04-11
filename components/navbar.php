51fa41962446dd88afe8b1d353e3dca7ca45c067
mampionona33 <ramamps33@gmail.com>
2023-04-11 11:00:35 +0300

    add exercice 3

---
+++ components/navbar.php
@@ -0,0 +1,7 @@
+<nav>
+    <ul>
+        <li><a href="../index.php">Home</a></li>
+        <li><a href="../pages/exercice_2.php" target="_blank">Exercice 2</a></li>
+        <li><a href="../pages/exercice_3.php" target="_blank">Exercice 3</a></li>
+    </ul>
+</nav>
