c48306d8a678e956e673fa5eb0151eae97ac8620
Mampionona33 <ramamps33@gmail.com>
2023-04-11 21:33:39 +0300

    chagne texte

--- src/pages/exercice_3.js
+++ src/pages/exercice_3.js
@@ -11,13 +11,13 @@ export default function Exercice3() {
         <thead>
           <tr>
             <td>
-              <h5>Titre 1</h5>{" "}
+              <h5>Titre 1</h5>
             </td>
             <td>
-              <h5>Titre 3</h5>{" "}
+              <h5>Titre 3</h5>
             </td>
             <td>
-              <h5>Titre 4</h5>{" "}
+              <h5>Titre 4</h5>
             </td>
           </tr>
         </thead>
@@ -28,13 +28,13 @@ export default function Exercice3() {
             <td>cellule simple</td>
           </tr>
           <tr>
-            <td colSpan={2}>Cellule double</td>
+            <td colSpan={2}>Cellule avec deux colonnes</td>
             <td>cellule simple</td>
           </tr>
           <tr>
             <td>cellule simple</td>
             <td>cellule simple</td>
-            <td rowSpan={2}>cellule simple</td>
+            <td rowSpan={2}>cellule deux lignes</td>
           </tr>
           <tr>
             <td>cellule simple</td>
@@ -43,7 +43,7 @@ export default function Exercice3() {
           <tr>
             <td>cellule simple</td>
             <td colSpan={2} rowSpan={2}>
-              cellule simple
+              cellule deux colonnes et deux lignes
             </td>
           </tr>
           <tr>
